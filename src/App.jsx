2858df9c7b25504fbd57256e6e1a9bbb65327f0d
Kanatbay <kana7292@gmail.com>
2024-06-14 15:55:37 +0500

    First Init

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route, RouterProvider } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
